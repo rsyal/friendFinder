c58e5dcb50a8975cd3b9dc9ed1a47500b584a6be
rsyal <35440553+rsyal@users.noreply.github.com>
2018-06-14 14:24:05 -0500

    Fixed the local host

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,6 +14,7 @@ var friends = require('../data/friends.js');
 // Export API routes
 
 module.exports = function(app) {
+	// console.log('__Enter apiroutes.js');
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
@@ -22,8 +23,8 @@ module.exports = function(app) {
  
 
 // Total list of friends, API GET Requests
-app.get("/api/friends", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+app.get('/api/friends', function(req, res) {
+	res.json(friends);
 });
 
 // API POST Requests
@@ -41,56 +42,62 @@ app.get("/api/friends", function(req, res) {
     
   // Capture the user input object
 		var userInput = req.body;
-		// console.log('userInput = ' + JSON.stringify(userInput));
+		
 
 		var userResponses = userInput.scores;
 		// console.log('userResponses = ' + userResponses);
 
 		// Compute best friend match
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 10000; // Make the initial value big for comparison
+		// var matchName = '';
+		// var matchImage = '';
+		// var totalDifference = 10000; // Make the initial value big for comparison
 
-		// Examine all existing friends in the list
-		for (var i = 0; i < friends.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
+		// // Examine all existing friends in the list
+		// for (var i = 0; i < friends.length; i++) {
+		// 	// console.log('friend = ' + JSON.stringify(friends[i]));
 
-			// Compute differenes for each question
-			var diff = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-			}
-			// console.log('diff = ' + diff);
+		// 	// Compute differenes for each question
+		// 	var diff = 0;
+		// 	for (var j = 0; j < userResponses.length; j++) {
+		// 		diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+		// 	}
+		// 	// console.log('diff = ' + diff);
 
-			// If lowest difference, record the friend match
-			if (diff < totalDifference) {
-				// console.log('Closest match found = ' + diff);
-				// console.log('Friend name = ' + friends[i].name);
-				// console.log('Friend image = ' + friends[i].photo);
+		// 	// If lowest difference, record the friend match
+		// 	if (diff < totalDifference) {
+		// 		// console.log('Closest match found = ' + diff);
+		// 		// console.log('Friend name = ' + friends[i].name);
+		// 		// console.log('Friend image = ' + friends[i].photo);
 
-				totalDifference = diff;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-			}
-		}
+		// 		totalDifference = diff;
+		// 		matchName = friends[i].name;
+		// 		matchImage = friends[i].photo;
+		// 	}
+		// }
 
-		// Add new user
-		friends.push(userInput);
+		// // Add new user
+		// friends.push(userInput);
 
 
-    // // Add user inputs to friends list
-      // $.post('/app.friends', userInput, function(data) {
-      //   friends.push(newfriend);
+    // Add user inputs to friends list
+      $.post('/app.friends', userInput, function(data) {
+        friends.push(userInput);
 
-      //   res.json(newfriend);
-      // 		// 	// Set the name and image values of friend match
-      // 			$('#userMatch').html(data.matchName);
-		  //   	$("#userMatchImage").attr("src", data.matchImage);
-		  //   	// Pop open the modal dialog
-      // 			$('#modal1').modal('open');
-      // 		});
-		// Send appropriate response
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+        res.json(userInput);
+      		// 	// Set the name and image values of friend match
+      	// 		$('#userMatch').html(data.matchName);
+		//     	$("#userMatchImage").attr("src", data.matchImage);
+		//     	// Pop open the modal dialog
+      	// 		$('#modal1').modal('open');
+      	// 	});
+		// // Send appropriate response
+		// res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 	});
+});
+
+
 };
+
+  
+
 
